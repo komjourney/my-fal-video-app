8625a1bf3cbe899e8bfcc3aee2d40fa39fbcf28b
komjourney <wumaoxian666@gmail.com>
2025-06-15 10:31:43 +0800

    删减模型

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 // app/page.js
-import VideoGenerator from '@/components/VideoGenerator'; // 导入我们刚写的组件
+
+import VideoGenerator from '@/components/VideoGenerator'; // 导入我们刚升级的组件
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
-       {/* 使用我们的组件 */}
+    <main className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
+      {/* 使用我们的组件 */}
       <VideoGenerator /> 
     </main>
   );
